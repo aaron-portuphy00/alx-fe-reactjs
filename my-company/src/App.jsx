3a8866f45a45f936453afdc5fbfb9d1c310a485d
aaronportuphy <aaronportuphy@gmail.com>
2024-11-10 21:18:04 +0000

    Updated App.jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './Home';
 import About from './About';
 import Services from './Services';
 import Contact from './Contact';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/navbar" element={<Navbar />} />
         </Routes>
       </div>
     </Router>
