a111d7f3902b6efe988d52ccb9fea62ef93ea0d0
aaronportuphy <aaronportuphy@gmail.com>
2024-12-01 21:58:45 +0000

    new update

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ function UserProfile() {
         <img
           src="https://via.placeholder.com/150"
           alt="User"
-          className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto hover:scale-110 transition-transform duration-300"
+          className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300"
         />
         <h1 className="text-lg md:text-xl text-blue-800 my-2 sm:my-4 hover:text-blue-500">
           John Doe
