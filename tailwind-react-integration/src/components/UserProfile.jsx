f46d706fbfb32686f689258697e1559b3ff04113
aaronportuphy <aaronportuphy@gmail.com>
2024-12-01 21:54:12 +0000

    corrected

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,13 +2,15 @@ import React from "react";
 
 function UserProfile() {
     return (
-      <div className="bg-gray-100 p-4 sm:p-8 max-w-xs sm:max-w-sm mx-auto my-10 sm:my-20 rounded-lg shadow-lg">
+      <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-10 sm:my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
         <img
           src="https://via.placeholder.com/150"
           alt="User"
-          className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto"
+          className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto hover:scale-110 transition-transform duration-300"
         />
-        <h1 className="text-lg sm:text-xl text-blue-800 my-2 sm:my-4">John Doe</h1>
+        <h1 className="text-lg sm:text-xl text-blue-800 my-2 sm:my-4 hover:text-blue-500">
+          John Doe
+        </h1>
         <p className="text-sm sm:text-base text-gray-600">
           Developer at Example Co. Loves to write code and explore new
           technologies.
@@ -18,5 +20,6 @@ function UserProfile() {
   }
   
   export default UserProfile;
+  
   
   
