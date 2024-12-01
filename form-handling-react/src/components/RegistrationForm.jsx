d32d778a948acd937c847ec645efe5a47ccadf86
aaronportuphy <aaronportuphy@gmail.com>
2024-12-01 23:00:11 +0000

    committed

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -4,10 +4,27 @@ function RegistrationForm() {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Form Data Submitted:', { username, email, password });
+    const newErrors = {};
+
+    if (!username) {
+      newErrors.username = 'Username is required';
+    }
+    if (!email) {
+      newErrors.email = 'Email is required';
+    }
+    if (!password) {
+      newErrors.password = 'Password is required';
+    }
+
+    setErrors(newErrors);
+
+    if (Object.keys(newErrors).length === 0) {
+      console.log('Form Data Submitted:', { username, email, password });
+    }
   };
 
   return (
@@ -18,9 +35,10 @@ function RegistrationForm() {
           type="text"
           id="username"
           name="username"
-          value={username} // Controlled value
+          value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
+        {errors.username && <p style={{ color: 'red' }}>{errors.username}</p>}
       </div>
       <div>
         <label htmlFor="email">Email:</label>
@@ -28,9 +46,10 @@ function RegistrationForm() {
           type="email"
           id="email"
           name="email"
-          value={email} // Controlled value
+          value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
+        {errors.email && <p style={{ color: 'red' }}>{errors.email}</p>}
       </div>
       <div>
         <label htmlFor="password">Password:</label>
@@ -38,9 +57,10 @@ function RegistrationForm() {
           type="password"
           id="password"
           name="password"
-          value={password} // Controlled value
+          value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        {errors.password && <p style={{ color: 'red' }}>{errors.password}</p>}
       </div>
       <button type="submit">Register</button>
     </form>
