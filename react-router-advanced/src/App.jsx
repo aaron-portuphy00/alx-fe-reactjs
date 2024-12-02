f4f3fb46d0dc286d0d6cd4620a0bbf83a1c203e6
aaronportuphy <aaronportuphy@gmail.com>
2024-12-02 00:00:23 +0000

    new

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import BlogPost from "./components/BlogPost";
+import Profile from "./components/Profile";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Home route */}
         <Route path="/" element={<Home />} />
-
-        {/* Dynamic route for blog posts */}
-        <Route path="/blog/:id" element={<BlogPost />} />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
