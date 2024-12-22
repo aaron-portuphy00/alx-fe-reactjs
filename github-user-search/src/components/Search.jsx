1f1c6dfac41db30c0d0f1e2854fb7670bddf822c
aaronportuphy <aaronportuphy@gmail.com>
2024-12-22 16:01:22 +0000

    committed

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -17,7 +17,7 @@ const Search = () => {
       const result = await fetchUserData(username);
       setUser(result);
     } catch (err) {
-      setError("Looks like we can't find the user");
+      setError("Looks like we cant find the user");
     } finally {
       setLoading(false);
     }
